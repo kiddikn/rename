7b6fc03c89beec979ecc65d2de42fe3dec69adb8
Kiddikn <kiddikn@gmail.com>
2013-05-07 09:03:48 +0900

    rename.rb

---
+++ rename.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+#./png_file/ の様に"/"まで入れる！
+prewd = ARGV[0]
+#変更前の形式と変更後の形式を指定したい
+puts "please type input file extension you have."
+    inPattern = STDIN.gets.gsub(/\n/,"")
+    #p inPattern
+puts "please type output file extension you want to have."
+    outPattern = STDIN.gets.gsub(/\n/,"")
+epsfolder = prewd + "eps-pic" 
+#指定されたディレクトリ内にeps専用のフォルダeps-picを作成
+system("mkdir #{epsfolder}")
+
+Dir::foreach(prewd) {|f|
+    if /png/ =~ f
+          nameEps = f.gsub(/#{inPattern}/,"#{outPattern}")
+         system("convert #{prewd}#{f} #{epsfolder}/#{nameEps}")
+    end
+}
