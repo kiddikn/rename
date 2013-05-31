a21965e4847e1f3f40c83a43b829f40687dd3ebe
Kiddikn <kiddikn@gmail.com>
2013-05-31 12:46:07 +0900

    ARGV[0]の無い場合に強制終了を追加

--- rename.rb
+++ rename.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 #./png_file/ の様に"/"まで入れる！
+if ARGV[0]==nil
+	puts "file directory is not found!\n"
+	exit! 
+end
 prewd = ARGV[0]
 #変更前の形式と変更後の形式を指定したい
 puts "please type input file extension you have."
@@ -12,8 +16,9 @@ epsfolder = prewd + "eps-pic"
 system("mkdir #{epsfolder}")
 
 Dir::foreach(prewd) {|f|
-    if /png/ =~ f
+    if /#{inPattern}/ =~ f
           nameEps = f.gsub(/#{inPattern}/,"#{outPattern}")
          system("convert #{prewd}#{f} #{epsfolder}/#{nameEps}")
     end
 }
+puts "work is done."
